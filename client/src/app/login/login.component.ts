56e66d8c7c65c0b4a29ca99817f9b8e671ad1ac9
Cale <cale.t.b@gmail.com>
2018-06-07 15:52:26 -0400

    Add initial server

--- client/src/app/login/login.component.ts
+++ client/src/app/login/login.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
+import { LoginService } from './login.service';
 
 @Component ({
 	selector: 'app-login',
 	templateUrl: './login.component.html',
-	styleUrls: ['./login.component.css']
+	styleUrls: ['./login.component.css'],
+	providers: [ LoginService ]
 })
+
 export class LoginComponent {
-	constructor(){ }
+	public user : User;
+
+	constructor(private loginService: Loginservice){
+		this.user = new User();
+	}
+	
+	validateLogin(){
+		if(this.user.username && this.user.password){
+			this.loginService.validateLogin(this.user).subscribe(result => {
+			console.log('Result is ',result);
+			}, error => {
+			console.log('Error is ', error);
+			});
+
+		} else {
+			alert('Enter username and password');
+		}
+	}
 }
