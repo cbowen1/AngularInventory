bbbe4bc7b6ba5e99e3016aa109d34e9b714ff1de
Cale <cale.t.b@gmail.com>
2018-06-08 15:27:24 -0400

    Added redirect to home working, need to finish html and css

--- client/src/app/login/login.component.ts
+++ client/src/app/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { LoginService } from './login.service';
 import { User } from '../models/user.model';
+import { Router } from '@angular/router';
 
 @Component ({
 	selector: 'app-login',
@@ -12,7 +13,7 @@ import { User } from '../models/user.model';
 export class LoginComponent {
 	public user : User;
 
-	constructor(private loginService: LoginService){
+	constructor(private loginService: LoginService, private router: Router){
 		this.user = new User();
 	}
 	
@@ -20,6 +21,11 @@ export class LoginComponent {
 		if(this.user.username && this.user.password){
 			this.loginService.validateLogin(this.user).subscribe(result => {
 			console.log('Result is ',result);
+			if(result['status'] === 'success') {
+				this.router.navigate(['/home']);
+			}else{
+				alert('That username and/or password do not match our records. Please try again.')
+			}
 			}, error => {
 			console.log('Error is ', error);
 			});
