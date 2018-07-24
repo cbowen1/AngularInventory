6bbb14684f00ab3b30239818c8aacd6152127c7c
Cale Bowen <cale.t.b@gmail.com>
2018-07-24 18:48:09 -0400

    Add the ability to read from the modal

--- client/src/app/add-post/add-post.component.ts
+++ client/src/app/add-post/add-post.component.ts
@@ -1,15 +1,31 @@
 import { Component } from '@angular/core';
 import { Post } from '../models/post.model';
+import { AddPostService } from './add-post.service';
  
 @Component({
   selector: 'app-add-post',
   templateUrl: './add-post.component.html',
-  styleUrls: ['./add-post.component.css']
+  styleUrls: ['./add-post.component.css'],
+  providers: [ AddPostService ]
 })
 export class AddPostComponent {
- 
-  constructor() {
-   
-  }
+
+	public post : Post;
+ 	
+ 	constructor(private addPostService: AddPostService) {
+   		this.post = new Post();
+  	}
+
+  	addPost() {
+  		if(this.post.title && this.post.description){
+  			//call the service method to add post
+  			this.addPostService.addPost(this.post).subscribe(res =>{
+  				//response from rest API call
+  				console.log('Response: ',res);
+  			});
+  		}else{
+  			alert('Title and Description required');
+  		}
+  	}
  
 }
